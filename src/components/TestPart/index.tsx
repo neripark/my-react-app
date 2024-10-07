41c0dd382f7a632e31e30890b0183e741326c447
neripark <neripark@gmail.com>
2024-10-07 17:18:07 +0900

    add jotai test

--- src/components/TestPart/index.tsx
+++ src/components/TestPart/index.tsx
@@ -1,5 +1,16 @@
+import { useAtomValue, useSetAtom } from "jotai";
+import { testAtom } from "../../atom"
+
 
 export const TestPart = () => {
+  const atomValue = useAtomValue(testAtom);
+  const setAtomValue = useSetAtom(testAtom);
+  const toggle = () => setAtomValue((prev) => !prev);
 
-  return (<div>Test Part</div>)
+  return (
+    <div>
+      <div>{atomValue.toString()}</div>
+      <button type="button" onClick={toggle}>toggle!</button>
+    </div>
+  );
 }
